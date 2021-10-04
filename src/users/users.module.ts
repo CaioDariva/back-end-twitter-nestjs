7442f6df098df9ddd05204a821bb83f235574355
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-04 20:57:23 -0300

    findUnique and call PrismaService in user.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  controllers: [UsersController, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
