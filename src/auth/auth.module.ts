241ee24b9f68b8194d58de366403794639e33848
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-05 20:59:08 -0300

    create jwt constnts and config auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './jwt.constants';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/jwt.constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'secretKeydeveserlongauns30caracteres',
+};
