08e035a677c82b2e64675f2ae6ddf09586eb3ef3
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-04 22:19:39 -0300

    validationpipe in main and class-validation in dto

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,5 +1,13 @@
+import { lsString, Length, IsString} from 'class-validator'
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 30)
   username: string;
+  @IsString()
+  @Length(8, 30)
   password: string;
+  @IsString()
+  @Length(2, 50)
   displayName: string;
 }
